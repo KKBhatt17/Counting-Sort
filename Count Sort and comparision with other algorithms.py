d6ffb468b2738ec8fbeea16c019ed2513c528c33
KKBhatt17 <49558229+KKBhatt17@users.noreply.github.com>
2022-03-16 23:14:52 +0530

    Add files via upload

---
+++ Count Sort and comparision with other algorithms.py
@@ -0,0 +1,123 @@
+# Python program for counting sort
+# which takes negative numbers as well
+from numpy.random import seed
+from numpy.random import randint
+import time
+import sys
+
+# The function that sorts the given arr[]
+def count_sort(arr):
+	max_element = int(max(arr))
+	min_element = int(min(arr))
+	range_of_elements = max_element - min_element + 1
+	# Create a count array to store count of individual
+	# elements and initialize count array as 0
+	count_arr = [0 for _ in range(range_of_elements)]
+	output_arr = [0 for _ in range(len(arr))]
+
+	# Store count of each character
+	for i in range(0, len(arr)):
+		count_arr[arr[i]-min_element] += 1
+
+	# Change count_arr[i] so that count_arr[i] now contains actual
+	# position of this element in output array
+	for i in range(1, len(count_arr)):
+		count_arr[i] += count_arr[i-1]
+
+	# Build the output character array
+	for i in range(len(arr)-1, -1, -1):
+		output_arr[count_arr[arr[i] - min_element] - 1] = arr[i]
+		count_arr[arr[i] - min_element] -= 1
+
+	# Copy the output array to arr, so that arr now
+	# contains sorted characters
+	for i in range(0, len(arr)):
+		arr[i] = output_arr[i]
+
+	return arr
+
+def mergeSort(arr):
+    if len(arr) > 1:
+  
+         # Finding the mid of the array
+        mid = len(arr)//2
+  
+        # Dividing the array elements
+        L = arr[:mid]
+  
+        # into 2 halves
+        R = arr[mid:]
+  
+        # Sorting the first half
+        mergeSort(L)
+  
+        # Sorting the second half
+        mergeSort(R)
+  
+        i = j = k = 0
+  
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+  
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+  
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    return arr
+
+# Driver program to test above function
+# seed random number generator
+seed(0)
+# generate some integers
+arr = randint(123, 961, 20000)
+brr = arr.copy()
+
+## count sort
+start_time = time.time()
+ans = count_sort(brr)
+print("--- %s seconds ---" % (time.time() - start_time))
+#print("Sorted character array is " + str(ans))
+
+
+# merge sort
+crr=arr.copy()
+drr=list(crr)
+start_time = time.time()
+err=mergeSort(drr)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# slection sort
+A=arr.copy()
+start_time = time.time()
+for i in range(len(A)):
+      
+    # Find the minimum element in remaining 
+    # unsorted array
+    min_idx = i
+    for j in range(i+1, len(A)):
+        if A[min_idx] > A[j]:
+            min_idx = j
+              
+    # Swap the found minimum element with 
+    # the first element        
+    A[i], A[min_idx] = A[min_idx], A[i]
+  
+print("--- %s seconds ---" % (time.time() - start_time))
+# Driver code to test above
+# print ("Sorted array")
+# for i in range(len(A)):
+#     print("%d" %A[i],end=" ") 
